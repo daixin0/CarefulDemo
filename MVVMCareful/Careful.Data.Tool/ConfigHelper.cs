30067f116f9f673e8eadcdab9267ba8f4414f3c3
Xin Dai <32857949+daixin10310@users.noreply.github.com>
2020-09-17 11:15:21 +0800

    框架设计变更

--- MVVMCareful/Careful.Data.Tool/ConfigHelper.cs
+++ MVVMCareful/Careful.Data.Tool/ConfigHelper.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Careful.Core.Tool
+namespace Careful.Data.Tool
 {
     public static class ConfigHelper
     {
--- MVVMCareful/Careful.Data.Tool/XmlHelper.cs
+++ MVVMCareful/Careful.Data.Tool/XmlHelper.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace Careful.Core.Tool
+namespace Careful.Data.Tool
 {
     public class XmlHelper
     {
