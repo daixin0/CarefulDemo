f3a50a0a4f1634b456d60ad96349a7241fbeadc2
daixin10310 <daixin1031017817@163.com>
2020-09-23 17:28:10 +0800

    优化代码

--- MVVMCareful/Careful.Core.PropertyValidation/VerificationAttribute.cs
+++ MVVMCareful/Careful.Core.PropertyValidation/VerificationAttribute.cs
@@ -6,17 +6,6 @@ using System.Text;
 
 namespace Careful.Core.PropertyValidation
 {
-    public enum VerificationType
-    {
-        [Description("不能为空")]
-        NotNull,
-        [Description("不能为小于0")]
-        PositiveNumber,
-        [Description("手机号")]
-        PhoneNumber,
-        [Description("不能少于6位")]
-        Password
-    }
     public class VerificationAttribute : Attribute
     {
         public VerificationType VerificationType { get; set; }
---
+++ MVVMCareful/Careful.Core.PropertyValidation/VerificationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Careful.Core.PropertyValidation
+{
+    public enum VerificationType
+    {
+        [Description("不能为空")]
+        NotNull,
+        [Description("不能为小于0")]
+        PositiveNumber,
+        [Description("手机号")]
+        PhoneNumber,
+        [Description("不能少于6位")]
+        Password
+    }
+}
