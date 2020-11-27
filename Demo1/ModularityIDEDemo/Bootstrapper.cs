53a95cb239d559279ce48a0ba9ae59fa36effd5d
代鑫 <daixin1031017817@163.com>
2020-11-27 15:20:55 +0800

    更新

--- Demo1/ModularityIDEDemo/Bootstrapper.cs
+++ Demo1/ModularityIDEDemo/Bootstrapper.cs
@@ -1,9 +1,14 @@
 ﻿using Careful.BootstrapperApplication;
+using Careful.Controls.MessageBoxControl;
+using Careful.Core.DialogServices;
+using Careful.Core.Ioc;
 using Microsoft.Practices.ServiceLocation;
 using Module.Test.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,14 +17,45 @@ namespace ModularityIDEDemo
 {
     public class Bootstrapper : CarefulBootstrapper
     {
-        //protected override DependencyObject CreateShell()
-        //{
-        //    return ServiceLocator.Current.GetInstance<MainWindowView>();
-        //}
+        private Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+            string[] files = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            foreach (var item in files)
+            {
+                var file = string.Format("{0}.dll", System.IO.Path.Combine(Path.GetDirectoryName(item), assemblyName.Name));
+
+                if (File.Exists(file))
+                {
+                    return Assembly.LoadFile(file);
+                }
+            }
+
+            return args.RequestingAssembly;
+        }
         public override void Run(bool runWithDefaultConfiguration)
         {
-            MainWindowView mainWindowView = new MainWindowView();
-            mainWindowView.ShowDialog();
+            AppDomain.CurrentDomain.AssemblyResolve -= OnAssemblyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+
+            Application application = null;
+            if (!CarefulIoc.Default.IsRegistered<Application>())
+            {
+                application = new Application();
+                application.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                application.Exit += Application_Exit;
+                ResourceDictionary controlTheme = (ResourceDictionary)Application.LoadComponent(
+                            new Uri("/Longfor.Controls;component/Theme.xaml", UriKind.Relative));
+
+                application.Resources.MergedDictionaries.Add(controlTheme);
+                CarefulIoc.Default.RegisterInstance<Application>(application);
+                CarefulIoc.Default.Register<IMessageView, MessageBoxWindow>(false);
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
         }
     }
 }
